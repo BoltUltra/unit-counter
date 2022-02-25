5db2f1549808acbde72447314d80be2ea8741618
BoltUltra <75336728+BoltUltra@users.noreply.github.com>
2022-02-25 08:39:28 +0000

    My first javascrip project

--- index.js
+++ index.js
@@ -1,5 +1,43 @@
-let valuee = prompt("Enter a Value");
-document.getElementById("value").textContent = valuee;
-document.getElementById("length").textContent = "This is Length";
-document.getElementById("volume").textContent = "This is Volume";
-document.getElementById("mass").textContent = "This is Mass";
+// Saved the number to a variable
+let valuee = parseInt(prompt("Enter a Value"));
+let input = document.getElementById("value");
+
+let meterToFeet = (valuee * 3.281).toFixed(3);
+let feetToMeter = (valuee / 3.281).toFixed(3);
+let literToGallon = (valuee / 4.546).toFixed(3);
+let gallonToLiter = (valuee * 4.546).toFixed(3);
+let kiloToPound = (valuee * 2.205).toFixed(3);
+let poundToKilo = (valuee * 2.205).toFixed(3);
+
+input.textContent = valuee;
+
+document.getElementById("length").textContent =
+  valuee +
+  " Meter(s) = " +
+  meterToFeet +
+  " Feet(s) | " +
+  valuee +
+  " Feet(s) = " +
+  feetToMeter +
+  " Meter(s)";
+
+document.getElementById("volume").textContent =
+  valuee +
+  " Liter(s) = " +
+  literToGallon +
+  " gallon(s) | " +
+  valuee +
+  " gallon(s) = " +
+  gallonToLiter +
+  " liter(s)";
+
+//   20 kilos = 44.092 pounds | 20 pounds = 9.072 kilos
+document.getElementById("mass").textContent =
+  valuee +
+  " kilo(s) = " +
+  kiloToPound +
+  " pound(s) | " +
+  valuee +
+  " pound(s) = " +
+  poundToKilo +
+  " kilo(s)";
