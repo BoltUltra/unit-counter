3f25e3334ee44fdf9e831ee0fe0b7b5320a63e50
BoltUltra <75336728+BoltUltra@users.noreply.github.com>
2022-02-25 06:46:48 +0000

    Styled the Converter

---
+++ index.js
@@ -0,0 +1,5 @@
+let valuee = prompt("Enter a Value");
+document.getElementById("value").textContent = valuee;
+document.getElementById("length").textContent = "This is Length";
+document.getElementById("volume").textContent = "This is Volume";
+document.getElementById("mass").textContent = "This is Mass";
