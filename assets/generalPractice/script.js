ad7d02bc1412671e993d6fa2cbdeb7914a8d735b
BoltUltra <75336728+BoltUltra@users.noreply.github.com>
2022-03-30 14:22:48 +0100

    ...

---
+++ assets/generalPractice/script.js
@@ -0,0 +1,21 @@
+function even_or_odd(number) {
+  if (number % 2 === 0) {
+    console.log("even");
+  } else {
+    console.log("odd");
+  }
+}
+
+even_or_odd(-1000);
+
+// function even_or_odd(number) {
+//   for (i = 0; i <= number; i++) {
+//     if (i % 2 === 0) {
+//       console.log("even");
+//     } else {
+//       console.log("odd");
+//     }
+//   }
+// }
+
+// even_or_odd(50);
