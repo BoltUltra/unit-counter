905e9a16ab5ab8d66cfae764042f86616aa86663
BoltUltra <75336728+BoltUltra@users.noreply.github.com>
2022-04-19 15:39:10 +0100

    finally finished the drumkit

--- assets/drumkit/index.js
+++ assets/drumkit/index.js
@@ -1,46 +1,48 @@
-// let calc = document.getElementById("calculator");
-let buttons = document.querySelectorAll(".drum");
-let numberOfButtons = document.querySelectorAll(".drum").length;
-let click = addEventListener("click", drumClick);
-let intro = new Audio("./sounds/intro.mp3");
+let drumButtons = document.querySelectorAll(".drum").length;
 
-for (i = 0; i < numberOfButtons; i++) {
-  buttons[i].click;
+for (let i = 0; i < drumButtons; i++) {
+  document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+    let drumButton = this.innerHTML;
+    makesound(drumButton);
+  });
 }
 
-function drumClick() {
-  // alert(Math.floor(Math.random() * 6));
-  intro.play();
+document.addEventListener("keypress", (event) => {
+  makesound(event.key);
+});
+
+function makesound(key) {
+  switch (key) {
+    case "w":
+      let sound1 = new Audio("./sounds/sound1.mp3");
+      sound1.play();
+      break;
+    case "a":
+      let sound2 = new Audio("./sounds/sound2.mp3");
+      sound2.play();
+      break;
+    case "s":
+      let sound3 = new Audio("./sounds/sound3.mp3");
+      sound3.play();
+      break;
+    case "d":
+      let sound4 = new Audio("./sounds/sound4.mp3");
+      sound4.play();
+      break;
+    case "j":
+      let sound5 = new Audio("./sounds/sound5.mp3");
+      sound5.play();
+      break;
+    case "k":
+      let sound6 = new Audio("./sounds/sound6.mp3");
+      sound6.play();
+      break;
+    case "l":
+      let sound7 = new Audio("./sounds/sound7.wav");
+      sound7.play();
+      break;
+
+    default:
+      break;
+  }
 }
-
-// Calculator Practice
-
-// function add(num1, num2) {
-//   return num1 + num2;
-// }
-
-// function subtract(num1, num2) {
-//   return num1 - num2;
-// }
-
-// function multiply(num1, num2) {
-//   return num1 * num2;
-// }
-
-// function divide(num1, num2) {
-//   return num1 / num2;
-// }
-
-// function divide(num1, num2) {
-//   return num1 / num2;
-// }
-
-// function mod(num1, num2) {
-//   return num1 % num2;
-// }
-
-// function calculator(num1, num2, operator) {
-//   return operator(num1, num2);
-// }
-
-// calc.textContent = Math.ceil(calculator(20, 3, divide)) + " approximately";
